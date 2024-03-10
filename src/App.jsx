836db4ea213d606df65a0c3de58428965b80fb99
unknown <rahultaakhr@gamil.com>
2024-03-10 19:38:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+ import './App.css'
+ import config from './config/config';
+ function App() {
+console.log(config.appwriteProjectId);
+
+  return (
+    <>
+     <h1> My first full stack project</h1>
+     <h2>HEllo RAhul i am a selgf taught
+      web developer no i want a remote job as a front end developer
+     </h2>
+    
+
+    </>
+  )
+}
+
+export default App
